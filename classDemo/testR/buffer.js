1189a029ab463ea827dabfbf37e595719efe5967
Amh <abaams78@gmail.com>
2017-09-07 08:56:28 -0500

    class demo buffer--

---
+++ classDemo/testR/buffer.js
@@ -0,0 +1,15 @@
+/*var buff = new Buffer('Hello', 'utf8');
+console.log(buff[2]);
+buff.write('wo');
+console.log(buff.toString());
+var bu = new Buffer('this is the string in my buffer');
+var slice = bu.slice(10,20);
+console.log(slice.toString());
+
+var fs = require('fs');
+var path = require('path');
+var greet = fs.readFileSync(path.join(__dirname + '/greet.txt'), 'utf8');
+console.log(greet);
+
+var greets = fs.readFile(path.join(__dirname + '/greet.txt'), 'utf8', (err, data)=> console.log(data));
+console.log('Done');*/
