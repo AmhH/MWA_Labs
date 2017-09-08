c1f1eea4d83b817d0ae7b95ba340353d4cac0ca8
Amh <abaams78@gmail.com>
2017-09-07 20:14:03 -0500

    lesson 4 demo1

---
+++ classDemo/node3/demo1.js
@@ -0,0 +1,12 @@
+/*var moment = require('moment');
+console.log(moment().format('MMMM, dddd, ha:mm'));*/
+const url = require('url');
+const my = url.parse('https://user:pass@sub.host.com:8080/p/a/t/h?query=string#hash', true);
+console.log(my);
+const urlO = {
+    protocol: 'https:',
+    host: 'www.mum.eddu',
+    search: '?q=CS572',
+    pathname: '/search'
+};
+console.log(url.format(urlO));
