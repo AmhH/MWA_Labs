7229c553b1a7ab236201d6ff6692bd0e3a7d18c5
Amh <abaams78@gmail.com>
2017-09-08 00:16:21 -0500

    lesson4 lab

---
+++ Lesson4/ex3.js
@@ -0,0 +1,14 @@
+const os = require('os');
+const Rx = require('@reactivex/rxjs');
+var checkSystem = new Promise(function (resolve, reject){
+        //console.log('Cheeking your system...');
+        if(os.cpus().length > 2 && os.totalmem() > 2e9){
+            resolve('System is checked successfully');
+        } else {
+            reject(os.cpus().length > 2 ? 'This app needs at least 2GB of RAM' : 'Processor is not supported');
+        }
+    });
+
+Rx.Observable.fromPromise(checkSystem)
+             .subscribe(e => console.log(e));
+console.log('Cheeking your system...');
